0d066e7c8586b68ac87e873d62b07b78c74bb139
Sergio Rezende <sergiorezende2001@gmail.com>
2022-09-02 20:08:50 -0300

    feat: Created main to do some tests

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "include/bptree.h"
+#include "include/cliente.h"
+
+int main() {
+
+    cliente * client1 = (cliente *) malloc(sizeof(cliente));
+    client1->id = 3;
+    strcpy(client1->nome, "Yancf");
+    strcpy(client1->nascimento, "05/07/2022");
+
+    BPTREE *tree = bptree_create("cliente", get_cliente_size(), 2, id_comparator, cliente_from_stream, cliente_to_stream, cliente_show);
+
+    return 0;
+}
