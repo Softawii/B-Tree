95f598b43e045e7923ba5becae4640694724e54d
Sergio Rezende <sergiorezende2001@gmail.com>
2022-09-02 20:07:24 -0300

    feat: Adding Includes to the project
    Created all the .h files needed.

---
+++ include/bptree.h
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include "core.h"
+
+#ifndef BPTREE_H
+#define BPTREE_H
+
+#define ROOT_INDEX 0
+
+typedef struct bptree {
+    FILE *data_file;
+    FILE *index_file;
+    FILE *meta_data_file;
+    int element_size;
+    int order;
+    comparator *comp;
+    from_stream *from;
+    to_stream *to;
+    show *show;
+} BPTREE;
+
+typedef struct bptree_data_node {
+    int m; // Number of elements in the node
+    int parent;
+    int next;
+    var *data;
+} BPTREE_DATA_NODE;
+
+typedef struct bptree_index_node
+{
+    int m; // Number of elements in the node
+    int flag;
+    int parent;
+    int *children;
+    int *keys;
+} BPTREE_INDEX_NODE;
+
+BPTREE * bptree_create(char *prefix_file_name, int element_size, int order, comparator * comp, from_stream * from, to_stream * to, show * show);
+int bp_tree_search(BPTREE *tree, int key);
+void bp_tree_insert(BPTREE *tree, int key, var element);
+void bp_tree_delete(BPTREE *tree, int key);
+
+#endif
