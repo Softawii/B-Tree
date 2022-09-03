0c8d79a45bf5706c74e1ecb0b3c747944ad56f61
Sergio Rezende <sergiorezende2001@gmail.com>
2022-09-02 22:26:47 -0300

    style: Changing order of code

--- src/bptree.c
+++ src/bptree.c
@@ -3,42 +3,6 @@
 #include "../include/bptree.h"
 #include "../include/core.h"
 
-void _set_data_node(BPTREE * tree, BPTREE_DATA_NODE * node, int index) {
-    fseek(tree->data_file, index * sizeof(BPTREE_DATA_NODE), SEEK_SET);
-    
-    fwrite(&node->m, sizeof(node->m), 1, tree->data_file);
-    
-    fwrite(&node->parent, sizeof(node->parent), 1, tree->data_file);
-    
-    fwrite(&node->next, sizeof(node->next), 1, tree->data_file);
-
-    for(int i = 0; i < 2*tree->order; i++) {
-        tree->to(tree->data_file, node->data[i]);
-    }
-}
-
-
-int _create_data_node(BPTREE *tree, int parent, int next) {
-    BPTREE_DATA_NODE *node = (BPTREE_DATA_NODE *) malloc(sizeof(BPTREE_DATA_NODE));
-    node->m = 0;
-    node->parent = parent;
-    node->next = next;
-    node->data = (var *) malloc(sizeof(var) * 2 * tree->order);
-    
-    for(int i = 0; i < 2 * tree->order; i++) {
-        node->data[i] = (char *) malloc(tree->element_size);
-        node->data[i] = "";
-    }
-
-    fseek(tree->data_file, 0, SEEK_END);
-    int index = ftell(tree->data_file) / sizeof(BPTREE_DATA_NODE);
-    _set_data_node(tree, node, index);
-
-    free(node);
-
-    return index;
-}
-
 void _set_index_node(BPTREE * tree, BPTREE_INDEX_NODE * node, int index) {
     fseek(tree->index_file, index * sizeof(BPTREE_INDEX_NODE), SEEK_SET);
     
@@ -80,6 +44,43 @@ int _create_index_node(BPTREE * tree, int parent, int flag) {
     free(node);
 }
 
+void _set_data_node(BPTREE * tree, BPTREE_DATA_NODE * node, int index) {
+    fseek(tree->data_file, index * sizeof(BPTREE_DATA_NODE), SEEK_SET);
+    
+    fwrite(&node->m, sizeof(node->m), 1, tree->data_file);
+    
+    fwrite(&node->parent, sizeof(node->parent), 1, tree->data_file);
+    
+    fwrite(&node->next, sizeof(node->next), 1, tree->data_file);
+
+    for(int i = 0; i < 2*tree->order; i++) {
+        tree->to(tree->data_file, node->data[i]);
+    }
+}
+
+
+int _create_data_node(BPTREE *tree, int parent, int next) {
+    BPTREE_DATA_NODE *node = (BPTREE_DATA_NODE *) malloc(sizeof(BPTREE_DATA_NODE));
+    node->m = 0;
+    node->parent = parent;
+    node->next = next;
+    node->data = (var *) malloc(sizeof(var) * 2 * tree->order);
+    
+    for(int i = 0; i < 2 * tree->order; i++) {
+        node->data[i] = (char *) malloc(tree->element_size);
+        node->data[i] = "";
+    }
+
+    fseek(tree->data_file, 0, SEEK_END);
+    int index = ftell(tree->data_file) / sizeof(BPTREE_DATA_NODE);
+    _set_data_node(tree, node, index);
+
+    free(node);
+
+    return index;
+}
+
+
 
 /**
  * @brief Creates a new B+ tree
@@ -100,13 +101,13 @@ BPTREE * bptree_create(char *prefix_file_name, int element_size, int order, comp
 
     BPTREE *tree = (BPTREE *) malloc(sizeof(BPTREE));
 
-    char * data_file_name = (char *) malloc(sizeof(char) * (strlen(prefix_file_name) + 9));
-    strcpy(data_file_name, prefix_file_name);
-    tree->data_file = fopen(strcat(data_file_name, "_data.dat"), "w+b");
-
     char * index_file_name = (char *) malloc(sizeof(char) * (strlen(prefix_file_name) + 10));
     strcpy(index_file_name, prefix_file_name);
     tree->index_file = fopen(strcat(index_file_name, "_index.dat"), "wb+");
+
+    char * data_file_name = (char *) malloc(sizeof(char) * (strlen(prefix_file_name) + 9));
+    strcpy(data_file_name, prefix_file_name);
+    tree->data_file = fopen(strcat(data_file_name, "_data.dat"), "w+b");
 
     char * meta_data_file_name = (char *) malloc(sizeof(char) * (strlen(prefix_file_name) + 9));
     strcpy(meta_data_file_name, prefix_file_name);
@@ -119,7 +120,9 @@ BPTREE * bptree_create(char *prefix_file_name, int element_size, int order, comp
     tree->to = to;
     tree->show = show;
 
-    _create_data_node(tree, -1, -1);
+    int root = _create_index_node(tree, -1, 1);
+
+    _create_data_node(tree, root, LEAF);
 
     _create_index_node(tree, -1, 1);
 
